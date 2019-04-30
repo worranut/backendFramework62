6152b81f41f373804b7a02c6aabb4537053865d9
beris <worranut.du@gmail.com>
2019-05-01 04:17:30 +0700

    Test commit to deploy.

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 
+// Test commit
 router.get("/", (req, res) => {
     res.send({
         status: "login"
